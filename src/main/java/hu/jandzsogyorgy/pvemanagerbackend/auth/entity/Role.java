2f086ca1d29d78b905edcb566f86ca1bf40a4581
György Jandzsó <haver1036@gmail.com>
2024-12-09 13:36:19 +0100

    Fix: mapping

--- src/main/java/hu/jandzsogyorgy/pvemanagerbackend/auth/entity/Role.java
+++ src/main/java/hu/jandzsogyorgy/pvemanagerbackend/auth/entity/Role.java
@@ -1,7 +1,9 @@
 package hu.jandzsogyorgy.pvemanagerbackend.auth.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+@Data
 public class Role {
     @Id
     private Long id;
