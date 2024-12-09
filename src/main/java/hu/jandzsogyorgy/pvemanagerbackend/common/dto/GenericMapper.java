7044658fbfaeca6d6d6e331a8c866f28a7a966d4
György Jandzsó <haver1036@gmail.com>
2024-12-09 15:57:05 +0100

    main: common classes

---
+++ src/main/java/hu/jandzsogyorgy/pvemanagerbackend/common/dto/GenericMapper.java
@@ -0,0 +1,4 @@
+package hu.jandzsogyorgy.pvemanagerbackend.common.dto;
+
+public interface GenericMapper<E, D> extends DtoMapper<E, D>, EntityMapper<E, D> {
+}
---
+++ src/main/java/hu/jandzsogyorgy/pvemanagerbackend/config/MappingConfig.java
@@ -0,0 +1,8 @@
+package hu.jandzsogyorgy.pvemanagerbackend.config;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface MappingConfig {
+}
