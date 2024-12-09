a47a984e2b2e350bc6438464e80ab9449f0a8d06
György Jandzsó <haver1036@gmail.com>
2024-12-09 12:07:37 +0100

    init

---
+++ src/main/java/hu/jandzsogyorgy/pvemanagerbackend/PveManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.jandzsogyorgy.pvemanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PveManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PveManagerBackendApplication.class, args);
+    }
+
+}
